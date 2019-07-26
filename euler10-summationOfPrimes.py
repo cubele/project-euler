4bb15e27e9df137b9fd9b6061f00c46dd3d7fede
cubele <43696912+cubele@users.noreply.github.com>
2019-07-26 18:03:27 +0900

    Correct Euler 10

--- euler10-summationOfPrimes.py
+++ euler10-summationOfPrimes.py
@@ -1,42 +1,66 @@
-import math
+# import math
 
-def isPrime(num):
-  for x in range(2, int(math.sqrt(num)) + 1):
-    if num % x == 0:
-      return False
-  return True
+# def isPrime(num):
+#   for x in range(2, int(math.sqrt(num)) + 1):
+#     if num % x == 0:
+#       return False
+#   return True
 
-N = 2000000
-sum = 0
-for num in range(2, N):
-  if isPrime(num):
-    sum += num
+# N = 2000000
+# sum = 0
+# for num in range(2, N):
+#   if isPrime(num):
+#     sum += num
 
-print(sum)
+# print(sum)
 
 
-def sumPrimes(n):
-  primes = list(range(2, n))
-  crossed = [False] * len(primes)
-  i = -1
-  while True:
-    currentPrimeIndex = i + 1
-    while crossed[currentPrimeIndex]:
-      currentPrimeIndex += 1
+class Primes:
+  def __init__(self, threshold):
+    self.output = ''
+
+    self.threshold = threshold
+    self.primes = list(range(2, threshold))
+    self.primesLen = len(self.primes)
+
+    self.crossed = [False] * self.primesLen
+
+    self.lastPrimeIndex = 0
+
+    self.cross()
+    while True:
+      nextIndex = self.nextPrimeIndex()
+      if not nextIndex:
+        return
+      self.cross
+
+  def cross(self):
+    prime = self.primes[self.lastPrimeIndex]
+    index = self.lastPrimeIndex + prime
+    while(index < self.primesLen):
+      self.crossed[index] = True
+      index += prime
+
+  def nextPrimeIndex(self):
+    index = self.lastPrimeIndex
+    while(True):
+      index += 1
+      if index >= self.primesLen:
+        return None
+      if not self.crossed[index]:
+        index
+    self.lastPrimeIndex = index
     
-    print(currentPrimeIndex)
-    currentPrime = primes[currentPrimeIndex]
+  def __str__(self):
+    return self.output
 
-    nextIndex = currentPrimeIndex
-    for p in range(1, n // currentPrime - 1):
-      nextIndex += currentPrime
-      crossed[nextIndex] = True
-      print(crossed)
+  def display(self):
+    for index in range(self.primesLen):
+      if self.crossed[index]:
+        print('x' + str(self.primes[index]), end=', ')
+      else:
+        print(str(self.primes[index]), end=', ')
+    
 
-  
-    if i > len(primes):
-      break
-
-    i = currentPrimeIndex + 1
-
-sumPrimes(10)
+euler10 = Primes(100)
+euler10.display()
